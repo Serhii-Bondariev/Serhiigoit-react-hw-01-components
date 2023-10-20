183dd480ed011be5b9769463711614f71e9d03fd
Serhii-Bondariev <xx.code4u@gmail.com>
2023-10-20 21:58:03 +0300

    finish

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,30 +1,18 @@
+// FriendList.jsx
+import React from 'react';
+import { FriendListItem } from '../FriendListItem/FriendListItem'; // Використовуємо іменований імпорт
 import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
     <section className={css.friendsSection}>
       <ul className={css.friendsList}>
-        {friends.map(({ id, avatar, name, isOnline }) => (
-          <div className={css.cardItem} key={id}>
-            <li className={css.item}>
-              <span
-                className={
-                  isOnline
-                    ? `${css.status} ${css.onLine}`
-                    : `${css.status} ${css.offLine}`
-                }
-              ></span>
-              <img
-                className={css.avatar}
-                src={avatar}
-                alt="User avatar"
-                width="48"
-              />
-              <p className={css.name}>{name}</p>
-            </li>
-          </div>
+        {friends.map(friend => (
+          <FriendListItem key={friend.id} friend={friend} />
         ))}
       </ul>
     </section>
   );
 };
+
+export default FriendList; // Залишаємо "export default" для FriendList
